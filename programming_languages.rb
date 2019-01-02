277ad7556b87d9b0cfea454f36aa9bffe2f11215
Jason Basuil <basuilj@gmail.com>
2019-01-02 00:22:56 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -9,7 +9,7 @@ def reformat_languages(languages)
       if new_hash[language] 
         new_hash[language][:style_hash] << style_hash
       else
-        new_hash[language]
+        new_hash[language] = 
     end
   end 
   binding.pry
