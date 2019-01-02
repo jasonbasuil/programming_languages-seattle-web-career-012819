0d1c4a3407e2bd48e12f4095b979e95464cbf734
Jason Basuil <basuilj@gmail.com>
2019-01-02 00:16:39 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  #iterates over the languages hash and builds a new hash that has the languages as keys that point to a value of a hash that describes the type and style
+  
+  new_hash = {}
+  languages.each do |style_hash, language_hash|
+    new_hash[language_hash] = nil
+    language_hash.each do |type, attribute|
+    end
+  end 
+  binding.pry
 end
