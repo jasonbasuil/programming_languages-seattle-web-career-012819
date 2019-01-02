8bcfed37402c395bfa13e54f8aa955933007c232
Jason Basuil <basuilj@gmail.com>
2019-01-02 00:21:47 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,7 +6,8 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style_hash, language_hash|
     language_hash.each do |language, type|
-      if new_hash[language] = attribute
+      if new_hash[language] 
+        new_hash[language][:]
     end
   end 
   binding.pry
