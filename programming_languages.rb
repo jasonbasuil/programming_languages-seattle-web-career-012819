05b2f8ac9a11bb7f34ad9ff9a6ba6abdf0594581
Jason Basuil <basuilj@gmail.com>
2019-01-02 00:29:40 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -17,3 +17,17 @@ require "pry"
   new_hash
   
 end"
+
+def reformat_languages(languages)
+  new_hash = {}
+  languages.each do |style, language_list|
+    language_list.each do |language, type|
+      if new_hash[language]
+        new_hash[language][:style] << style
+      else
+        new_hash[language] = {:type => type[:type], :style => [style]}
+      end
+    end
+  end
+  new_hash
+end
